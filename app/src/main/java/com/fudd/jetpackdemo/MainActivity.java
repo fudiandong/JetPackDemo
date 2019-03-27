c3fe104ff4e97021f1fa05a4374ce1aa63624d40
fudd <fudd6@chinaunicom.cn>
2019-03-27 14:40:09 +0800

    add btn click

--- app/src/main/java/com/fudd/jetpackdemo/MainActivity.java
+++ app/src/main/java/com/fudd/jetpackdemo/MainActivity.java
@@ -7,30 +7,50 @@ import android.widget.Toast;
 
 import com.fudd.jetpackdemo.lifecycle.LifeCycleActivity;
 
-import java.util.List;
-
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
-import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.btn1)
-    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
     }
 
-    @OnClick(R.id.btn1)
-    public void toLifeCycle() {
-        Toast.makeText(MainActivity.this,"btn1",Toast.LENGTH_SHORT).show();
-        LifeCycleActivity.startLifeCycleActivity(this);
+    @OnClick({R.id.btn1,R.id.btn2,R.id.btn3,R.id.btn4,R.id.btn5,R.id.btn6})
+    public void toLifeCycle(View view) {
+
+        switch (view.getId()){
+            case R.id.btn1:
+                Toast.makeText(MainActivity.this,"btn1",Toast.LENGTH_SHORT).show();
+                LifeCycleActivity.startLifeCycleActivity(this);
+                break;
+            case R.id.btn2:
+                Toast.makeText(MainActivity.this,"btn2",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn3:
+                Toast.makeText(MainActivity.this,"btn3",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn4:
+                Toast.makeText(MainActivity.this,"btn4",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn5:
+                Toast.makeText(MainActivity.this,"btn5",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn6:
+                Toast.makeText(MainActivity.this,"btn6",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
     }
+
+
+
 
 }
